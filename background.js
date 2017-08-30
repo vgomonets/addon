9fc428af9167b7355e13137727c5949375e22b9e
Ігор <rafalskiy@users.noreply.github.com>
2017-08-30 12:46:09 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 window.addEventListener("message", function (event) {
+    console.log('message', event);
     if (event.data &&
         event.data.direction == "open-my-page" && event.data.message == 'open-tab') {
         browser.tabs.create();
