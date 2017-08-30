3587b0e471d40c50c6e12824c3c326d50c5d2820
Viktor <vh@div-art.com>
2017-08-30 11:22:43 +0300

    first

---
+++ background.js
@@ -0,0 +1,24 @@
+/*
+Open a new tab, and load "my-page.html" into it.
+*/
+function openMyPage() {
+    console.log("injecting");
+    // browser.tabs.create({
+    //   "url": "/my-page.html"
+    // });
+
+    window.addEventListener("message", function (event) {
+        if (event.data &&
+            event.data.direction == "open-my-page" && event.data.message == 'open-tab') {
+            browser.tabs.create({
+                "url": "/my-page.html"
+            });
+        }
+    });
+}
+
+/*
+Add openMyPage() as a listener to clicks on the browser action.
+*/
+browser.browserAction.onClicked.addListener(openMyPage);
+
